23a7c0b9061b5242b379bb73892e8e91900be9c1
orky161 <orky161@gmail.com>
2020-12-28 18:29:35 +0200

    add library.io auth package

--- src/routes/clients.js
+++ src/routes/clients.js
@@ -1,32 +1,33 @@
 require("express-async-errors")
 const express = require('express')
 const {login, getClient, updateClient, getClients, removeBook, addBook,} = require("../controller/clients")
-const {permission_mw, auth_mw, equalId_mw} = require("../auth")
-
+const {libraryAuth} = require("../config/index")
+const {verifyPermission, decodeToken, equalField} = libraryAuth
+const {LibraryRoles} = require("../../../library.io-libs/dist/roles");
 
 const router = express.Router()
 
-router.get("/", [auth_mw, permission_mw("ADMIN")], (async (req, res) => {
+router.get("/", [decodeToken, verifyPermission(LibraryRoles.ADMIN)], (async (req, res) => {
     const clients = await getClients()
     res.status(200).json({clients})
 }))
 
-router.get("/:id", [auth_mw, permission_mw("ADMIN"), equalId_mw()], (async (req, res) => {
+router.get("/:id", [decodeToken, verifyPermission(LibraryRoles.ADMIN), equalField("id")], (async (req, res) => {
     const client = await getClient(req.params.id)
     res.status(200).json({client})
 }))
 
-router.put("/:id", [auth_mw, permission_mw("ADMIN"), equalId_mw()], (async (req, res) => {
+router.put("/:id", [decodeToken, verifyPermission(LibraryRoles.ADMIN), equalField("id")], (async (req, res) => {
     await updateClient(req.params.id, req.body)
     res.status(202).json({message: "success"})
 }))
 
-router.post("/:id/book/:bookId", [auth_mw, permission_mw("ADMIN"), equalId_mw()], (async (req, res) => {
+router.post("/:id/book/:bookId", [decodeToken, verifyPermission(LibraryRoles.ADMIN), equalField("id")], (async (req, res) => {
     await addBook(req.params.id, req.params.bookId)
     res.status(202).json({message: "success"})
 }))
 
-router.delete("/:id/book/:bookId", [auth_mw, permission_mw("ADMIN"), equalId_mw()], (async (req, res) => {
+router.delete("/:id/book/:bookId", [decodeToken, verifyPermission(LibraryRoles.ADMIN), equalField("id")], (async (req, res) => {
     await removeBook(req.params.id, req.params.bookId)
     res.status(202).json({message: "success"})
 }))
